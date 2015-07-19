8794a3a175a6291a2fcd824b6b0720666e0c495f
Florent SANLAVILLE <florent-pro@hotmail.com>
2015-07-19 12:29:27 +0200

    main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fsanlavi <florent-pro@hotmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/07/18 18:44:56 by fsanlavi          #+#    #+#             */
-/*   Updated: 2015/07/18 23:24:35 by fsanlavi         ###   ########.fr       */
+/*   Updated: 2015/07/19 12:28:07 by fsanlavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void	display_sudoku(int **sudoku)
 	int x;
 	int y;
 
-	y = 0;
-	while (y < 9)
+	x = 0;
+	while (x < 9)
 	{
-		x = 0;
-		while (x < 9)
+		y = 0;
+		while (y < 9)
 		{
 			ft_putchar('0' + sudoku[x][y]);
-			if (y == 8)
+			if (y != 8)
 				ft_putchar(' ');
-			x++;
+			y++;
 		}
 		ft_putchar('\n');
-		y++;
+		x++;
 	}
 }
 
@@ -40,19 +40,17 @@ int		**read_args(char **argv)
 	int x;			// ligne
 	int y;			// colone
 	int **sudoku;	// tableau de int
-	
+
 	sudoku = (int **)malloc(sizeof(int *) * 9);
 	x = 0;
 	while (x < 9)
 	{
-		printf(" -- x=%d\n", x);
-		sudoku[x] = (int *)malloc(sizeof(int) * 9);
-		y = 0;
 		if (ft_strlen(argv[x]) != 9)
 			return (NULL);
+		sudoku[x] = (int *)malloc(sizeof(int) * 9);
+		y = 0;
 		while (y < 9)
 		{
-			printf("y=%d\n",y); 
 			if (argv[x][y] == '.')
 				sudoku[x][y] = 0;
 			else if (argv[x][y] >= '1' && argv[x][y] <= '9')
@@ -63,7 +61,6 @@ int		**read_args(char **argv)
 		}
 		x++;
 	}
-	printf("read_args(): done\n");
 	return (sudoku);
 }
 
@@ -74,15 +71,17 @@ int		main(int argc, char **argv)
 	if (argc == 10)
 	{
 		sudoku = read_args(&argv[1]);
-		printf("salut\n");
-		/*if (sudoku == NULL)
+		printf("read_args()\n");
+		if (sudoku == NULL)
 		{
 			ft_putstr("Erreur\n");
 			return (160);
-		}*/
-		printf("bonjoir");
-		printf("salutttt");
+		}
+		printf("===== START =====\n");
+		display_sudoku(sudoku);
+		printf("===== SOLVE =====\n");
 		solve(sudoku);
+		printf("solve(): done\n");
 		display_sudoku(sudoku);
 	}
 	else
